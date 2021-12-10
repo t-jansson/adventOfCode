d11d3aa3638083c8a2d65d9915c6013d95423044
t-jansson <t-jansson@outlook.com>
2021-12-10 14:42:41 +0100

    Started day2-2

---
+++ Day2/day2-2.py
@@ -0,0 +1,25 @@
+#Advent of Code 2021 Day 2 - 1
+
+import os, traceback
+
+with open('Input.txt') as input:
+    commands = input.readlines()
+
+depth = int(0)
+position = int(0)
+for command in commands:
+    if('forward' in command):
+        #print('add value to position')
+        line = command.split(' ')
+        position += int(line[1])
+    if('down' in command):
+        #print('decrease position')
+        line = command.split(' ')
+        depth += int(line[1])
+    if('up' in command):
+        #print('increase position')
+        line = command.split(' ')
+        depth -= int(line[1])
+
+print('Depth:', depth, 'Position:', position)
+print('Result:', int(depth) * int(position))
