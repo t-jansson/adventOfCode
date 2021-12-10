fae49fc23f9c8a9b7dc7363f6263826bb44f8c90
t-jansson <t-jansson@outlook.com>
2021-12-10 11:26:43 +0100

    Day1 part 1

---
+++ Day1/day1-1.py
@@ -0,0 +1,18 @@
+
+
+import os, traceback
+
+try:
+    with open('Input.txt') as input:
+        depths = input.readlines()
+
+    prevValue = 0
+    increases = 0
+    for depth in depths:
+        if(prevValue != 0 and int(depth) > int(prevValue)): increases += 1
+        prevValue = depth
+
+    print(increases)
+
+except Exception:
+    traceback.print_exc()
