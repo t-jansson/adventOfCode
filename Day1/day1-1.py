406c015ed2a2a351e096a910b92c1923bd58c29e
t-jansson <t-jansson@outlook.com>
2021-12-10 14:29:09 +0100

    Removed try, catch.

--- Day1/day1-1.py
+++ Day1/day1-1.py
@@ -2,17 +2,13 @@
 
 import os, traceback
 
-try:
-    with open('Input.txt') as input:
-        depths = input.readlines()
+with open('Input.txt') as input:
+    depths = input.readlines()
 
-    prevValue = 0
-    increases = 0
-    for depth in depths:
-        if(prevValue != 0 and int(depth) > int(prevValue)): increases += 1
-        prevValue = depth
+prevValue = 0
+increases = 0
+for depth in depths:
+    if(prevValue != 0 and int(depth) > int(prevValue)): increases += 1
+    prevValue = depth
 
-    print(increases)
-
-except Exception:
-    traceback.print_exc()
+print(increases)
--- Day1/day1-2.py
+++ Day1/day1-2.py
@@ -2,19 +2,15 @@
 
 import os, traceback
 
-try:
-    with open('Input.txt') as input:
-        depths = input.readlines()
+with open('Input.txt') as input:
+    depths = input.readlines()
 
-    prevValue = int(0)
-    increases = int(0)
-    for i in range(0, len(depths) -2):
-        curVal = int(depths[i]) + int(depths[i+1]) + int(depths[i+2])
-        #print(int(depths[i]), int(depths[i+1]), int(depths[i+2]), '=', curVal)
-        if(prevValue != 0 and curVal > prevValue): increases += 1
-        prevValue = curVal
+prevValue = int(0)
+increases = int(0)
+for i in range(0, len(depths) -2):
+    curVal = int(depths[i]) + int(depths[i+1]) + int(depths[i+2])
+    #print(int(depths[i]), int(depths[i+1]), int(depths[i+2]), '=', curVal)
+    if(prevValue != 0 and curVal > prevValue): increases += 1
+    prevValue = curVal
 
-    print('Result:', increases)
-
-except Exception:
-    traceback.print_exc()
+print('Result:', increases)
