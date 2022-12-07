4570aa3465f9284bdd5400abb6252e434a1e5297
t-jansson <t-jansson@outlook.com>
2022-12-07 16:16:39 +0100

    Day6-2 working

--- 2022/Day6/day6-2.py
+++ 2022/Day6/day6-2.py
@@ -1,13 +1,13 @@
-#Advent of Code 2022 Day 6 - 1
+#Advent of Code 2022 Day 6 - 2
 
 with open('input.txt') as input:
     buffer = input.readline()
     
 
-for i in range(4,len(buffer)):
-    SOP = buffer[i-4:i]
+for i in range(14,len(buffer)):
+    SOP = buffer[i-14:i]
     SOP = set(SOP)
-    if len(SOP) == 4:
+    if len(SOP) == 14:
         marker = i
         break
 
