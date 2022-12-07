095d960278f2a8d0211e7b61912128f0e3e9dfe1
t-jansson <t-jansson@outlook.com>
2022-12-07 15:41:48 +0100

    Minor improvement to day5-1

--- 2022/Day5/day5-1.py
+++ 2022/Day5/day5-1.py
@@ -13,12 +13,11 @@ for line in input:
         i = 0
         for j in range(0, len(line), 4):
             if not line[j].isspace():
-                columns[i].insert(0, line[j:j+3])
+                columns[i].insert(0, line[j+1:j+2])
             i += 1
     elif line[0:4] == 'move': #Move crates
         moves = line.split()
         for i in range(int(moves[1])): #How many crates to move
-            #columns[int(line[17])-1].append(columns[int(line[12])-1].pop())
             columns[int(moves[5])-1].append(columns[int(moves[3])-1].pop())
     else:
         print('Init', columns)
@@ -28,4 +27,4 @@ topBoxes = []
 for row in columns:
     if len(row) != 0:
         topBoxes.append(row.pop())
-print('Code', topBoxes)
+print('Code:', ''.join(topBoxes))
