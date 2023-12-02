451eb1d256f7260d260a67269f8ec1322fbd75e3
Tommy Jansson <t-jansson@outlook.com>
2023-12-02 12:43:34 +0100

    Day2-1 working

---
+++ 2023/Day2/day2-1.py
@@ -0,0 +1,42 @@
+#Advent of Code 2023 Day 2 - 1
+
+with open('input.txt') as input:
+    lines = input.readlines()
+
+sum = 0
+for line in lines:
+    line = line.split(":")
+    gameID = int(line[0].split()[1])
+    print(gameID)
+
+    sets = line[1].split(";")
+    for set in sets:
+        red = 0
+        green = 0
+        blue = 0
+
+        colors = set.split(",")
+
+        for color in colors:
+            split = color.split()
+            if split[1] == "red":
+                red = int(split[0])
+            elif split[1] == "green":
+                green = int(split[0])
+            elif split[1] == "blue":
+                blue = int(split[0])
+    
+        if(red <= 12 and green <= 13 and blue <= 14):
+            print("Possible")
+            print(set)
+            possible = True
+        else:
+            print("Impossible")
+            print(set)
+            possible = False
+            break
+        
+    if possible:
+        sum += gameID
+
+print("SUM:", sum)
