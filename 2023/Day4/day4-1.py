253ad1f18df416e4eaf2b03441cd130f112c9145
Tommy Jansson <tommy.jansson@plejd.com>
2023-12-04 08:50:44 +0100

    Day4-1 working

---
+++ 2023/Day4/day4-1.py
@@ -0,0 +1,24 @@
+#Advent of Code 2023 Day 4 - 1
+
+with open('input.txt') as input:
+    lines = input.readlines()
+
+sum = 0
+for line in lines:
+    line = line.split(":")
+    card = line[0]
+    line = line[1].split("|")
+    numbers = line[0].split()
+    winningNumbers = line[1].split()
+
+    cardValue = 0
+    for number in numbers:
+        try:
+            if winningNumbers.index(number) >= 0:
+                if cardValue == 0: cardValue = 1
+                else: cardValue *= 2
+        except ValueError:
+            pass
+    sum += cardValue
+
+print(sum)
