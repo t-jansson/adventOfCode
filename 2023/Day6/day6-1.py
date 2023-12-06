39f4e2a036c0803afcab27cba36cf69910f2fca5
Tommy Jansson <tommy.jansson@plejd.com>
2023-12-06 13:15:48 +0100

    Day6-2 working

--- 2023/Day6/day6-1.py
+++ 2023/Day6/day6-1.py
@@ -1,13 +1,34 @@
 #Advent of Code 2023 Day 6 - 1
 
+import numpy
+
+def calc(time, distance):
+    wins = []
+    for x in range(1,time+1):
+        if x * (time - x) > distance:
+            wins.append(x)
+    return len(wins)
+
 with open('input.txt') as input:
     times = input.readline()
     times = times.split(":")[1]
     times = times.split()
+    times = [int(i) for i in times]
     distances = input.readline()
     distances = distances.split(":")[1]
     distances = distances.split()
+    distances = [int(i) for i in distances]
+
+
 
 print("Times:", times)
 print("Distances:", distances)
 
+wins = []
+for x in range(0, len(times)):
+    wins.append(calc(times[x], distances[x]))
+
+result = numpy.prod(wins)
+
+print(wins)
+print(result)
---
+++ 2023/Day6/day6-2.py
@@ -0,0 +1,30 @@
+#Advent of Code 2023 Day 6 - 2
+
+def calc(time, distance):
+    wins = 0
+    for x in range(1,time+1):
+        if x * (time - x) > distance:
+            wins += 1
+    return wins
+
+with open('input.txt') as input:
+    times = input.readline()
+    times = times.split(":")[1]
+    times = times.split()
+    distances = input.readline()
+    distances = distances.split(":")[1]
+    distances = distances.split()
+
+time = ""
+for x in times:
+    time += x
+distance = ""
+for x in distances:
+    distance += x
+
+print("Times:", time)
+print("Distances:", distance)
+
+win = calc(int(time), int(distance))
+
+print(win)
