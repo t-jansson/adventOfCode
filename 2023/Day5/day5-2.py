1a403a9901b66f07fcd9fe4af23f5f6eb2ecf17a
Tommy Jansson <tommy.jansson@plejd.com>
2023-12-08 16:38:07 +0100

    Day7-1 working

--- 2023/Day5/day5-2.py
+++ 2023/Day5/day5-2.py
@@ -1,8 +1,4 @@
-#Advent of Code 2023 Day 5 - 2 not working
-
-import threading, os
-
-MINLOCATIONS = [0,0,0,0,0,0,0,0,0,0]
+#Advent of Code 2023 Day 5 - 1
 
 def getInfo(lines, index):
     data = []
@@ -19,81 +15,42 @@ def getInfo(lines, index):
 def calc(input, dataList):
     output = 0
     for data in dataList:
-        if int(data[1]) <= input and input <= int(data[1]) + int(data[2]):
-            output = int(data[0]) + (input - int(data[1]))
+        sourceMin = int(data[1])
+        souceMax = int(data[1]) + int(data[2])
+        
+        if sourceMin <= input and input <= souceMax:
+            output = int(data[0]) + (input - sourceMin)
             break
-    if output == 0: return input
+    if output == 0: output = input
     return output
 
-def calcLocation():
-    minLocation = 0
-    start = int(seeds[int(threading.current_thread().name)*2])
-    stop = int(seeds[int(threading.current_thread().name)*2]) + int(seeds[int(threading.current_thread().name)*2+1])
-    for y in range(start, stop): #Tar för lång tid...
-        # print(y)
-        #seed = int(seed)
-        soil = calc(y, seed_to_soil)
-        fertilizer = calc(soil, soil_to_fertilizer)
-        water = calc(fertilizer, fertilizer_to_water)
-        light = calc(water, water_to_light)
-        temperature = calc(light, light_to_temperature)
-        humidity = calc(temperature, temperature_to_humidity)
-        location = calc(humidity, humidity_to_location)
-        # print(seed, location)
-        if minLocation == 0 or location < minLocation:
-            minLocation = location
-    print("Thread", threading.current_thread().name, "done:", minLocation)        
-    MINLOCATIONS[int(threading.current_thread().name)] = minLocation
 
-if __name__ == "__main__":
+with open('input.txt') as input:
+    lines = input.readlines()
 
-    with open('input.txt') as input:
-        lines = input.readlines()
+seeds = lines[0].split(":")[1].split()
 
-    seeds = lines[0].split(":")[1].split()
+index = 3
+seed_to_soil, index = getInfo(lines, index)
+soil_to_fertilizer, index = getInfo(lines, index)
+fertilizer_to_water, index = getInfo(lines, index)
+water_to_light, index = getInfo(lines, index)
+light_to_temperature, index = getInfo(lines, index)
+temperature_to_humidity, index = getInfo(lines, index)
+humidity_to_location, index = getInfo(lines, index)
 
-    index = 3
-    seed_to_soil, index = getInfo(lines, index)
-    soil_to_fertilizer, index = getInfo(lines, index)
-    fertilizer_to_water, index = getInfo(lines, index)
-    water_to_light, index = getInfo(lines, index)
-    light_to_temperature, index = getInfo(lines, index)
-    temperature_to_humidity, index = getInfo(lines, index)
-    humidity_to_location, index = getInfo(lines, index)
+locations = []
+for seed in seeds:
+    # print(seed)
+    seed = int(seed)
+    soil = calc(seed, seed_to_soil)
+    fertilizer = calc(soil, soil_to_fertilizer)
+    water = calc(fertilizer, fertilizer_to_water)
+    light = calc(water, water_to_light)
+    temperature = calc(light, light_to_temperature)
+    humidity = calc(temperature, temperature_to_humidity)
+    location = calc(humidity, humidity_to_location)
+    # print(seed, location)
+    locations.append(location)
 
-
-    t0 = threading.Thread(target=calcLocation, name="0")
-    t1 = threading.Thread(target=calcLocation, name="1")
-    t2 = threading.Thread(target=calcLocation, name="2")
-    t3 = threading.Thread(target=calcLocation, name="3")
-    t4 = threading.Thread(target=calcLocation, name="4")
-    t5 = threading.Thread(target=calcLocation, name="5")
-    t6 = threading.Thread(target=calcLocation, name="6")
-    t7 = threading.Thread(target=calcLocation, name="7")
-    t8 = threading.Thread(target=calcLocation, name="8")
-    t9 = threading.Thread(target=calcLocation, name="9")
-
-    t0.start()
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
-    t9.start()
-
-    t0.join()
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    t5.join()
-    t6.join()
-    t7.join()
-    t8.join()
-    t9.join()
-
-    print("Min?", min(MINLOCATIONS))
-    print("the end")
+print(min(locations))
---
+++ 2023/Day7/day7-1.py
@@ -0,0 +1,91 @@
+#Advent of Code 2023 Day 7 - 1
+
+
+def cardToValue(card):
+    match card:
+        case "A": return 14
+        case "K": return 13
+        case "Q": return 12
+        case "J": return 11
+        case "T": return 10
+        case _: return int(card)
+
+def assignValueToHand(hand):
+    most_common = max(hand, key=hand.count)
+    count = hand.count(most_common)
+    match count:
+        case 5: return 7
+        case 4: return 6
+        case 3: 
+            #Check full house
+            hand = hand.replace(most_common,"")
+            most_common = max(hand, key=hand.count)
+            if hand.count(most_common) == 2: return 5
+            else: return 4
+        case 2:
+            #Check two pair
+            hand = hand.replace(most_common,"")
+            most_common = max(hand, key=hand.count)
+            if hand.count(most_common) == 2: return 3
+            else: return 2
+        case 1: return 1
+        case _: return 0
+
+def compareHands(h1, h2):
+    if h1["value"] > h2["value"]: return h1
+    if h1["value"] < h2["value"]: return h2
+    for i in range(0,len(h1["hand"])):
+        if cardToValue(h1["hand"][i]) > cardToValue(h2["hand"][i]): return h1
+        if cardToValue(h1["hand"][i]) < cardToValue(h2["hand"][i]): return h2
+    print("THE SAME!!!")
+    return "THE SAME"
+
+def sortHands(hands):
+    for i in range(0,len(hands)):
+        for j in range(i+1,len(hands)):
+            if hands[i] != compareHands(hands[i], hands[j]): swapPositions(hands, i, j)
+            # output.insert(0, compareHands(hands[i],hands[j]))
+    # return output
+
+def swapPositions(list, p1, p2):
+    ls = list[p1], list[p2]
+    list[p2], list[p1] = ls
+    return list
+
+def getValueFromHand(hand):
+    return hand["value"]
+
+if __name__ == "__main__":
+
+    with open('input.txt') as input:
+        input = input.readlines()
+        input = [x.split() for x in input]
+
+    # print(input)
+
+    hands = []
+    for cards, rank in input:
+        # print(cards, rank)
+        hand = {}
+        hand["hand"] = cards
+        hand["rank"] = int(rank)
+        hand["value"] = assignValueToHand(cards)
+        # print(hand["value"])
+        # print(hand)
+    
+        hands.append(hand)
+
+    # print(hands)
+    # for i in range(0,len(hands)-1):
+    #     print(compareHands(hands[i],hands[i+1])["hand"])
+    
+    #hands.sort(key=getValueFromHand, reverse=False)
+    sortHands(hands)
+    hands.reverse()
+    # print(hands)
+    result = 0
+    for i in range(0, len(hands)):
+        print(i+1, hands[i], hands[i]["rank"] * (i+1))
+        result += hands[i]["rank"] * (i+1)
+
+    print(result)
