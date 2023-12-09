04c867181c52232c9de986c85ce52b71fa10cdc6
Tommy Jansson <t-jansson@outlook.com>
2023-12-09 22:09:45 +0100

    working on day8-2

--- 2023/Day8/day8-2.py
+++ 2023/Day8/day8-2.py
@@ -1,5 +1,21 @@
 #Advent of Code 2023 Day 8 - 2
 
+A = []
+B = []
+C = []
+
+def getNewLeftPos(pos):
+    return B[A.index(pos)]
+
+def getNewRightPos(pos):
+    return C[A.index(pos)]
+
+def checkZ(pos):
+    return pos[2] == "Z"
+
+def getA(str):
+    return str[0:3]
+
 if __name__ == "__main__":
 
     with open('input.txt') as input:
@@ -7,9 +23,7 @@ if __name__ == "__main__":
         input.readline() #Read empty line
         input = input.readlines()
 
-    A = []
-    B = []
-    C = []
+    input.sort(key=getA)
     for data in input:
         line = {}
         A.append(data[0:3])
@@ -21,27 +35,22 @@ if __name__ == "__main__":
         if pos[2] == 'A':
             positions.append(pos)
 
-    # pos = 'AAA' #Start location
-    target = '**Z'
     steps = 0
     while(1):
+        # print(positions, steps)
         for direction in directions:
             # print(positions, steps)
-            allZ = True 
             steps += 1
-            for i, pos in enumerate(positions):
-                index = A.index(pos)        
-                # print(direction, pos, steps)
-                if direction == "L":
-                    pos = B[index]
-                else: #direction == "R":
-                    pos = C[index]
-                positions[i] = pos
-                # if pos[2] == "Z": 
-                #     print(pos)
-                if allZ: allZ = pos[2] == "Z"
-            if allZ: break
-        if allZ: break
+            # dir = direction
+            if direction == "L":
+                positions = list(map(getNewLeftPos, positions))
+            else:
+                positions = list(map(getNewRightPos, positions))
+            done = all(list(map(checkZ, positions)))
+            if done: break
+            # if steps % 10000 == 0: print(steps, positions)
+        if done: break
 
+    print(positions)
     print("Steps", steps)
         
