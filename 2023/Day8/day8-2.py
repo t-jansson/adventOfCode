3bab4a157d24db738243bede2f3a6a31711f127f
Tommy Jansson <t-jansson@outlook.com>
2023-12-09 23:09:32 +0100

    Day8-2 faster but not working

--- 2023/Day8/day8-2.py
+++ 2023/Day8/day8-2.py
@@ -1,20 +1,26 @@
 #Advent of Code 2023 Day 8 - 2
 
-A = []
-B = []
-C = []
+import time
+network = {}
+
 
 def getNewLeftPos(pos):
-    return B[A.index(pos)]
+    return network[pos][0]
 
 def getNewRightPos(pos):
-    return C[A.index(pos)]
+    return network[pos][1]
 
 def checkZ(pos):
     return pos[2] == "Z"
 
 def getA(str):
     return str[0:3]
+
+def checkDone(poss):
+    for pos in poss:
+        if pos[2] != "Z":
+            return False
+    return True
 
 if __name__ == "__main__":
 
@@ -23,32 +29,35 @@ if __name__ == "__main__":
         input.readline() #Read empty line
         input = input.readlines()
 
+    #Create dictionalry of the network
     input.sort(key=getA)
-    for data in input:
-        line = {}
-        A.append(data[0:3])
-        B.append(data[7:10])
-        C.append(data[12:15])
+    for i, data in enumerate(input):
+        network[data[0:3]] = [data[7:10], data[12:15]]
 
+    #Get start positions
     positions = []
-    for pos in A:
-        if pos[2] == 'A':
-            positions.append(pos)
+    for key in network.keys():
+        if key[2] =="A":
+            positions.append(key)
 
+    startTime = time.time()
     steps = 0
     while(1):
         # print(positions, steps)
         for direction in directions:
             # print(positions, steps)
             steps += 1
-            # dir = direction
             if direction == "L":
-                positions = list(map(getNewLeftPos, positions))
+                positions = [network[p][0] for p in positions]
+                # positions = list(map(getNewLeftPos, positions))
             else:
-                positions = list(map(getNewRightPos, positions))
-            done = all(list(map(checkZ, positions)))
+                positions = [network[p][1] for p in positions]
+                # positions = list(map(getNewRightPos, positions))
+            # done = all(list(map(checkZ, positions)))
+            # done = all([p[2] == "Z" for p in positions])
+            done = checkDone(positions)
             if done: break
-            # if steps % 10000 == 0: print(steps, positions)
+            # if steps % 100000 == 0: print(steps, positions, time.time()-startTime)
         if done: break
 
     print(positions)
