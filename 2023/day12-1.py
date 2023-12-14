f981ec1d60e87d8913237280c9f059deb3a4cacb
Tommy Jansson <t-jansson@outlook.com>
2023-12-14 22:16:07 +0100

    Day12-1 working

--- 2023/day12-1.py
+++ 2023/day12-1.py
@@ -2,6 +2,37 @@
 
 import re
 
+matches = set()
+
+def solve(record, hashes, row):
+    
+    index = [i.start() for i in re.finditer("\?", row)]
+    # for i in index:
+    if len(index) == 0:
+        if row.count("#") != hashes:
+            return
+        x = re.search(record, row)
+        if x is not None:
+            # print(row)
+            matches.add(row)
+        return
+    if index[0] < len(row):
+        newStr = row[:index[0]] + "#" + row[index[0]+1:]
+        # print(newStr)
+        solve(record, hashes, newStr)
+        newStr = row[:index[0]] + "." + row[index[0]+1:]
+        # print(newStr)
+        solve(record, hashes, newStr)
+    else:
+        newStr = row[:index[0]] + "#"
+        # print(newStr)
+        solve(record, hashes, newStr)
+        newStr = row[:index[0]] + "."
+        # print(newStr)
+        solve(record, hashes, newStr)
+            
+
+
 if __name__ == "__main__":
 
     with open('day12_input.txt') as input:
@@ -10,17 +41,19 @@ if __name__ == "__main__":
     rows = [d[0] for d in data]
     records = [d[1].split(",") for d in data]
 
+    result = 0
     for row, record in zip(rows, records):
         print(row, record)
         index = 0
-        for rec in record:
+        sub = ""
+        hashes = 0
+        for i, rec in enumerate(record):
             rec = int(rec)
-            sub = "[?#]" * rec + "[?.]"
-            print(row[index:])
-            x = re.search(sub, row[index:])
-            if x == None:
-                print("No match")
-                break
-            i, j = x.span()
-            index += j
-            print(i,j)
+            hashes += rec
+            sub += "[#]" * rec
+            if i < len(record)-1:
+                sub += "[.]+"
+        solve(sub, hashes, row)
+        result += len(matches)
+        matches.clear()
+    print(result)
