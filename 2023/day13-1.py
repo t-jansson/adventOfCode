231ba931bf30a848973f77eb7814e400a409ea84
Tommy <t-jansson@outlook.com>
2023-12-15 17:17:32 +0100

    started day13

---
+++ 2023/day13-1.py
@@ -0,0 +1,11 @@
+#Advent of Code 2023 Day 13 - 1
+
+if __name__ == "__main__":
+
+    with open('day13_input.txt') as input:
+        data = [d.strip() for d in input.readlines()]
+
+for row in data:
+    print(row)
+
+for row in data:
