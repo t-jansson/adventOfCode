981e813bbb0c286b11b714736e6c16b9d11a7939
Tommy Jansson <t-jansson@outlook.com>
2023-12-16 21:46:12 +0100

    Working on day13-1

--- 2023/day13-1.py
+++ 2023/day13-1.py
@@ -1,11 +1,57 @@
 #Advent of Code 2023 Day 13 - 1
 
+from numpy import transpose
+    
+def findReflection(dataSet):
+    for i in range(len(dataSet)-1):
+        # print(dataSet[i])
+        if dataSet[i] == dataSet[i+1]:
+            # print("Reflection at row", i)
+            k = 1
+            for j in range(i+1, len(dataSet)):
+                if j-k < 0:
+                    print("out of range in dataset", j-k)
+                elif dataSet[j] == dataSet[j-k]:
+                    # print("Reflection", j, j-k)
+                    if j == len(dataSet)-1 or j-k == 0:
+                        print("Real reflection at", i, i+1)
+                        return i + 1
+                    else:
+                        k += 2
+                        continue
+                else:
+                    break
+    return 0
+    
+
 if __name__ == "__main__":
 
     with open('day13_input.txt') as input:
-        data = [d.strip() for d in input.readlines()]
+        input = [i.strip() for i in input.readlines()]
 
-for row in data:
-    print(row)
+    dataSets = []
+    data = []
+    for i, row in enumerate(input):
+        row = row.replace("#", "1")
+        row = row.replace(".", "0")
+        row = [int(x) for x in row]
+        if row == []:
+            dataSets.append(data)
+            data = []
+        elif i == len(input) -1 :
+            dataSets.append(data)
+        else:
+            data.append(row)
 
-for row in data:
+    result = 0
+    for i, dataSet in enumerate(dataSets):
+        setResult = 0
+        setResult += findReflection(dataSet) * 100
+        dataSet = transpose(dataSet).tolist()
+        setResult += findReflection(dataSet)
+        print(setResult, i)
+        if setResult == 0:
+            print("Fault? at ", i)
+        result += setResult
+
+    print(result)
